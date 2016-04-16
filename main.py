cb77a6f413fb1659e262b21cb2b1b1b7cbf929d8
Shalin Luo <luoshalin@LuoShalins-MBP.wv.cc.cmu.edu>
2016-04-15 22:48:23 -0400

    completed all experiements, before cleaning the code

--- main.py
+++ main.py
@@ -15,7 +15,9 @@ from forwardBackword import ll_decode
 def main(argv):
     # x = sys.argv[2]
     # hmm_train_file_path = '../../data/hmm_train_data'
-    hmm_train_file_path = '../../data/hmm_test_data'
+    # hmm_train_file_path = '../../data/hmm_test_data'
+    # hmm_train_file_path = '../../data/hmm_train_data_jpn'
+    hmm_train_file_path = '../../data/hmm_test_data_jpn'
     vtb_train_file_path = '../../data/viterbi_train_data'
     # F-B stopping criteria
     threshold = 1e-5
@@ -28,44 +30,43 @@ def main(argv):
 
     # forward & backward algorithms
     index_dic, A, B = gen_matrix()
-    # pcll_old = -1000.0
-    # pcll_new = -1000.0
-    # pcll_list = []
-    # itr = 1
-    # while True:
-    #     alpha_table, pcll_alpha = forward(input_str, A, B, index_dic)
-    #     beta_table, pcll_beta = backward(input_str, A, B, index_dic)
-    #     A, B = forward_backward(alpha_table, beta_table, A, B, index_dic, input_str)
-    #
-    #     # update pcll
-    #     pcll_new = pcll_alpha
-    #     pcll_list.append(pcll_new)
-    #     if abs(pcll_new - pcll_old) < threshold:
-    #         break
-    #
-    #     print 'ITERATION#' + str(itr) + ': ' + str(pcll_new)
-    #
-    #     # update
-    #     pcll_old = pcll_new
-    #     itr += 1
+    pcll_old = -1000.0
+    pcll_new = -1000.0
+    pcll_list = []
+    itr = 1
+    while True:
+        alpha_table, pcll_alpha = forward(input_str, A, B, index_dic)
+        beta_table, pcll_beta = backward(input_str, A, B, index_dic)
+        A, B = forward_backward(alpha_table, beta_table, A, B, index_dic, input_str)
+
+        # update pcll
+        pcll_new = pcll_alpha
+        pcll_list.append(pcll_new)
+        if abs(pcll_new - pcll_old) < threshold:
+            break
+
+        print 'ITERATION#' + str(itr) + ': ' + str(pcll_new)
+
+        # update
+        pcll_old = pcll_new
+        itr += 1
 
     # plot
     # plot_pcll(pcll_list)
 
-    # print '\n=========FINAL A & B==========='
-    # print 'A:'
-    # print A
-    # print '\nB:'
-    # print B
-    # print '==============================\n'
+    print '\n=========FINAL A & B==========='
+    print 'A:'
+    print A
+    print '\nB:'
+    print B
+    print '==============================\n'
 
 
     # viterbi decoder
-    vtb_input_str = preprocess(vtb_train_file_path)
-    vtb_hidden_state_list = vtb_decode(vtb_input_str, A, B, index_dic)
-    ll_hidden_state_list = ll_decode(vtb_input_str, B, index_dic)
-
-    print ll_hidden_state_list
+    # vtb_input_str = preprocess(vtb_train_file_path)
+    # vtb_hidden_state_list = vtb_decode(vtb_input_str, A, B, index_dic)
+    # ll_hidden_state_list = ll_decode(vtb_input_str, B, index_dic)
+    # print ll_hidden_state_list
 
 
     print 'END!'
